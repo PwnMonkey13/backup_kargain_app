d978b28851c1cad7ccb4b89c21ad61d8864e1b94
Nicolas Giraudo <giraudo.nicolas13@gmail.com>
2020-06-01 17:49:27 +0200

    tags

--- src/components/Tags/TagsControlled.js
+++ src/components/Tags/TagsControlled.js
@@ -1,23 +1,50 @@
-import React, { useEffect } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TagsInput from '../../components/Tags/TagsInput';
+import React, { useEffect, useState } from 'react';
+import Tags from './Tags';
+import PropTypes from 'prop-types';
+import ValidationError from '../Form/Validations/ValidationError';
 
-export default ({ name, rules, control, errors, ...props }) => {
-
-    const getTags = (tags) => {
-        control.setValue(name, tags);
-    };
+const TagsControlled = ({ name, rules, control, register, errors }) => {
+    const [tags, setTags] = useState([]);
 
     useEffect(() => {
-        control.register(name, []);
+        control.register(name, rules);
+        const tags = control.getValues(name);
+        setTags(tags);
     }, []);
 
     return (
-        <div>
-            <CssBaseline/>
-            <TagsInput
-                fireTags={getTags}
-            />
-        </div>
+        <>
+            <div className="d-flex flex-column">
+
+                <input
+                    name="test"
+                    type="text"
+                    // ref={(e) => register(e, rules)}
+                />
+
+                <Tags
+                    defaultTags={tags}
+                    onChange={tags => {
+                        setTags(tags);
+                        control.setValue(name, tags);
+                    }}
+                />
+            </div>
+            {errors && <ValidationError errors={errors} name={name}/>}
+        </>
     );
-}
+};
+
+TagsControlled.propTypes = {
+    name: PropTypes.string.isRequired,
+    control: PropTypes.any.isRequired,
+    defaultValues: PropTypes.arrayOf(PropTypes.string),
+    maxTags: PropTypes.number,
+};
+
+TagsControlled.defaultProps = {
+    fireTags: () => {
+    },
+};
+
+export default TagsControlled;
---
+++ src/components/Tags/TagsList.js
@@ -0,0 +1,59 @@
+import React, {memo} from 'react';
+import PropTypes from 'prop-types'
+import clsx from 'clsx';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles(() => ({
+    tags: {
+        display: 'inline-flex',
+        flexWrap: 'wrap',
+        width: '100%',
+        margin: 0,
+    },
+
+    tag: {
+        display: 'flex',
+        alignItems: 'center',
+        listStyle: 'none',
+        margin: '2px',
+        padding: '5px',
+        backgroundColor: 'gainsboro',
+        borderRadius: '10px',
+    },
+}));
+
+const TagsList = memo(({tags, editMode, onRemoveTag}) => {
+    const classes = useStyles()
+    if(!tags) return null
+
+    return(
+        <ul className={clsx(classes.tags)}>
+            {tags.map((tag, i) => (
+                <li key={i} className={classes.tag}>
+                    #{tag}
+
+                    {editMode && (
+                        <span onClick={() => {
+                            onRemoveTag(i);
+                        }}>
+                        <RemoveCircleIcon/>
+                    </span>
+                    )}
+                </li>
+            ))}
+        </ul>
+    )
+})
+
+TagsList.propTypes = {
+    tags : PropTypes.arrayOf(PropTypes.string),
+    editMode : PropTypes.bool,
+    onRemoveTag : PropTypes.func
+}
+
+TagsList.defaultProps = {
+    tags : []
+}
+
+export default TagsList
