d8866cf4cae81dcbe483934f91729871a2a77666
Nicolas Giraudo <giraudo.nicolas13@gmail.com>
2020-06-10 04:17:34 +0200

    stripe

--- src/routes/dev/stripe.js
+++ src/routes/dev/stripe.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
-
-import CheckoutForm from './CheckoutForm';
+import CheckoutForm from '../../components/Stripe/CheckoutForm';
 
 const stripeToken = "pk_test_51GqJrJEItcAGSRw8the6YZdACyYMrHOZsCRKSfNr6tJRlN4L3MpXpUjo7MOpAPvPcpY5WvIxDSwsZRH5JTKU5q9a00dDJhABpd";
 
