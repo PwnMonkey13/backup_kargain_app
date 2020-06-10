9107467d1b052d2bd8c17e4506228cd74014c2fc
Nicolas Giraudo <giraudo.nicolas13@gmail.com>
2020-06-10 04:17:43 +0200

    repo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withPlugins = require('next-compose-plugins');
+const withSourceMaps = require('@zeit/next-source-maps')();
 const withImages = require('next-images');
 const path = require('path');
-require('dotenv').config();
+const packageJson = require('./package');
+const date = new Date();
 
 const nextBundleAnalyzer = ({ enabled = true }) => (nextConfig = {}) => ({
     ...nextConfig,
@@ -32,15 +34,51 @@ const nextBundleAnalyzer = ({ enabled = true }) => (nextConfig = {}) => ({
 });
 
 const nextConfig = {
-    distDir: "dist/client",
+    distDir: 'dist/client',
     // Have to list all the environment variables used here to make it available to the client side code
     env: {
         GOOGLE_STATIC_API_KEY: process.env.GOOGLE_STATIC_API_KEY,
-        GOOGLE_SSO_CLIENT_ID : process.env.GOOGLE_SSO_CLIENT_ID,
-        FACEBOOK_SSO_APP_ID : process.env.FACEBOOK_SSO_APP_ID,
+        GOOGLE_SSO_CLIENT_ID: process.env.GOOGLE_SSO_CLIENT_ID,
+        FACEBOOK_SSO_APP_ID: process.env.FACEBOOK_SSO_APP_ID,
+        SENTRY_DSN: process.env.SENTRY_DSN,
+        BUILD_TIME: date.toString(),
+        BUILD_TIMESTAMP: +date,
+        APP_STAGE: process.env.APP_STAGE,
+        APP_NAME: packageJson.name,
+        APP_VERSION: packageJson.version,
     },
-    webpack: (config, options) => {
-        config.resolve.alias['~'] = path.resolve(__dirname);
+    webpack: (config, { isServer, buildId }) => {
+        const APP_VERSION_RELEASE = `${packageJson.version}_${buildId}`;
+
+        // Dynamically add some "env" variables that will be replaced during the build
+        config.plugins[0].definitions['process.env.APP_RELEASE'] = JSON.stringify(buildId);
+        config.plugins[0].definitions['process.env.APP_VERSION_RELEASE'] = JSON.stringify(APP_VERSION_RELEASE);
+
+        if (isServer) { // Trick to only log once
+            console.debug(`[webpack] Building release "${APP_VERSION_RELEASE}"`);
+        }
+
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: 'empty',
+        };
+
+        // XXX See https://github.com/zeit/next.js/blob/canary/examples/with-sentry-simple/next.config.js
+        // In `pages/_app.js`, Sentry is imported from @sentry/node. While
+        // @sentry/browser will run in a Node.js environment, @sentry/node will use
+        // Node.js-only APIs to catch even more unhandled exceptions.
+        //
+        // This works well when Next.js is SSRing your page on a server with
+        // Node.js, but it is not what we want when your client-side bundle is being
+        // executed by a browser.
+        //
+        // Luckily, Next.js will call this webpack function twice, once for the
+        // server and once for the client. Read more:
+        // https://nextjs.org/docs#customizing-webpack-config
+        //
+        // So ask Webpack to replace @sentry/node imports with @sentry/browser when
+        // building the browser's bundle
+
         config.resolve.mainFields = ['main', 'browser', 'module'];
         config.module.rules.push(
             {
@@ -60,13 +98,17 @@ const nextConfig = {
                         },
                     },
                 ],
-            });
+            },
+        );
+
         return config;
     },
+    poweredByHeader: 'NRN - With love',
 };
 
+console.debug(`Building Next with NODE_ENV="${process.env.NODE_ENV}" APP_STAGE="${process.env.APP_STAGE}" for CUSTOMER_REF="${process.env.CUSTOMER_REF}"`);
+
 module.exports = withPlugins([
-    [withImages, {
-        exclude: path.resolve(__dirname, 'public/images/svg'),
-    }],
+    [withSourceMaps],
+    [withImages, { exclude: path.resolve(__dirname, 'public/images/svg') }],
 ], nextConfig);
