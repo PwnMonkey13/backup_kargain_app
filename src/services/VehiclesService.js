a902ff178d7f115b665616a85c8e5e36591f0ca5
Nicolas Giraudo <giraudo.nicolas13@gmail.com>
2020-08-11 19:23:59 +0200

    vehicles

--- src/services/VehiclesService.js
+++ src/services/VehiclesService.js
@@ -1,69 +1,53 @@
 import fetch from 'isomorphic-unfetch';
-import config from '../../config/config';
-import handleResponse from '../../libs/handleResponse';
+import config from '../config/config';
+import handleResponse from '../libs/handleResponse';
 const queryString = require('query-string');
 
-function buildUrl (url, params) {
+function buildUrl (url, params = {}) {
     return Object.keys(params).length ? `${url}?${queryString.stringify(params)}` : url;
 }
 
-function getMakes (makes) {
-    let params = {};
-    let makesParams = null;
-    if (makes && Array.isArray(makes)) {
-        makesParams = makes.filter(make => typeof make === 'string').join(',');
-        params = {
-            ...params,
-            filter: makesParams,
-        };
-    }
-
-    const url = buildUrl(`${config.api}/vehicles/internal/motorcycles/makes`, params);
+function getMakes (vehicleType) {
+    const url = buildUrl(`${config.api}/vehicles/${vehicleType}/makes`);
     return fetch(url)
         .then(handleResponse)
-        .then(json => {
-            return json.data;
-        })
+        .then(json => json.data)
         .catch(err => {
             throw err;
         });
 }
 
-function getMakeModels (make) {
-    if (!make) throw 'make param is required';
-    const url = `${config.api}/vehicles/internal/motorcycles/${make}/models`;
+function getMakeModels (vehicleType, makeID) {
+    if (!makeID) throw 'make param is required';
+    const url = `${config.api}/vehicles/${vehicleType}/${makeID}/models`;
 
     return fetch(url)
         .then(handleResponse)
-        .then(json => {
-            return json.data;
-        })
+        .then(json => json.data)
         .catch(err => {
             throw err;
         });
 }
 
-function getCarGenerations (make_id, model_id) {
+function getVehiclesGenerations (make_id, model_id) {
     if (!make_id || isNaN(make_id)) throw 'make param is not a number';
     if (!model_id || isNaN(model_id)) throw 'make param is not a number';
 
     const params = {
         select: 'generation',
         make_id,
-        model_id,
+        model_id
     };
     const url = buildUrl(params);
     return fetch(url)
         .then(handleResponse)
-        .then(json => {
-            return json.data;
-        })
+        .then(json => json.data)
         .catch(err => {
             throw err;
         });
 }
 
-function getCarYearsVersion (make_id, model_id, generation_id) {
+function getVehiclesYearsVersion (make_id, model_id, generation_id) {
     if (!make_id || isNaN(make_id)) throw 'make param is not a number';
     if (!model_id || isNaN(model_id)) throw 'make param is not a number';
     if (!generation_id || isNaN(generation_id)) throw 'make param is not a number';
@@ -71,14 +55,12 @@ function getCarYearsVersion (make_id, model_id, generation_id) {
         select: 'year',
         make_id,
         model_id,
-        generation_id,
+        generation_id
     };
     const url = buildUrl(params);
     return fetch(url)
         .then(handleResponse)
-        .then(json => {
-            return json.data;
-        })
+        .then(json => json.data)
         .catch(err => {
             throw err;
         });
@@ -87,6 +69,6 @@ function getCarYearsVersion (make_id, model_id, generation_id) {
 export default {
     getMakes,
     getMakeModels,
-    getCarGenerations,
-    getCarYearsVersion,
+    getVehiclesGenerations,
+    getVehiclesYearsVersion
 };
